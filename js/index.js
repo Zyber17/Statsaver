1c1a8979aa8235abb659e680b6fa6dfb004845d0
Zackary Corbett <zyber17@gmail.com>
2016-05-15 22:50:28 -0400

    Added weather initialization function.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
-	initalizePanels();
-	setTimeout(refreshPanels(), 87000); // 1.45 Mins, set to just say withing the totally free Forecast.io free tier
+	weatherInit();
+	panels();
+	setTimeout(panels(), 87000); // 1.45 Mins, set to just say withing the totally free Forecast.io free tier
 });
 
-function initalizePanels() {
-	busInit();
-	trainInit();
-	weatherInit();
-}
-
-function refreshPanels() {
-	busRefresh();
-	trainRefresh();
-	weatherRefresh();
+function panels() {
+	bus();
+	train();
+	weather();
 }
