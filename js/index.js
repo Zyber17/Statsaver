9c08ef37b18bdb45d63998445a6f3950a37035c3
Zackary Corbett <zyber17@gmail.com>
2016-05-15 20:13:33 -0400

    Initial version

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-	alert(window.long)
+	initalizePanels();
+	setTimeout(refreshPanels(), 87000); // 1.45 Mins, set to just say withing the totally free Forecast.io free tier
 });
+
+function initalizePanels() {
+	busInit();
+	trainInit();
+	weatherInit();
+}
+
+function refreshPanels() {
+	busRefresh();
+	trainRefresh();
+	weatherRefresh();
+}
