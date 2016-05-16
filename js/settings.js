44698a01b1bee2530ba0d8f1e3e8be1a87c808c3
Zackary Corbett <zyber17@gmail.com>
2016-05-15 20:28:59 -0400

    Added settings for bus and train schedules

--- js/settings.js
+++ js/settings.js
@@ -2,3 +2,11 @@
 window.forcastAPIKey = '';
 window.lat = '';
 window.long = '';
+
+// Train settings
+window.ctaTrainKey = '';
+window.trainStop = '';
+
+// Bus settings
+window.ctaBusKey = '';
+window.busStop = '';
