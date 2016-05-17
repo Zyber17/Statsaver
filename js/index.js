771dd586c5ad2bc2baf4c1b3a62a67055bbb6fb9
Zackary Corbett <zyber17@gmail.com>
2016-05-17 19:19:57 -0400

    Fixed auto refresh functionality.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	weatherInit();
 	panels();
-	setTimeout(panels(), 87000); // 1.45 Mins, set to just say withing the totally free Forecast.io free tier
 });
 
 function panels() {
 	bus();
-	train();
+	// train();
 	weather();
+	note();
+	var refresh = setTimeout(function(){panels();}, 87000); // 1.45 Mins, set to just say withing the totally free Forecast.io free tier
 }
