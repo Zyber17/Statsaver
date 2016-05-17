1b98077eabbb35d3d76f93cc8446a097b9f402ca
Zackary Corbett <zyber17@gmail.com>
2016-05-17 19:20:54 -0400

    Colored the Skycons somewhat

--- js/panels/weather.js
+++ js/panels/weather.js
@@ -1,12 +1,26 @@
+var skycons;
+
 function weatherInit() {
-
+	skycons = new Skycons({
+		"monochrome": false,
+		"colors" : {
+			"main": "black",
+			"cloud": "blue",
+			"sun": "#F0F",
+		}
+	});
+	skycons.add('skycon', Skycons.PARTLY_CLOUDY_NIGHT);
+	skycons.play();
 }
 
-function weatherRefresh() {
-
-}
-
-function weatherGet() {
+function weather() {
 	var url = 'https://api.forecast.io/forecast/' + window.forcastAPIKey + '/' + window.lat + ',' + window.long;
-	
+	$.getJSON(url, function(data) {
+		$('#temp').html(Math.round(data.currently.temperature));
+		$('#high').html(Math.round(data.daily.data[0].temperatureMin));
+		$('#low').html(Math.round(data.daily.data[0].temperatureMax));
+		$('#precip').html(Math.round(data.currently.precipProbability * 100));
+		skycons.set('skycon', data.currently.icon);
+	});
+
 }
