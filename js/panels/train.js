3cbc1eca103393929dd00bb50154a61b93eb1971
Zackary Corbett <zyber17@gmail.com>
2016-05-21 20:15:41 -0400

    Initial train setup.

--- js/panels/train.js
+++ js/panels/train.js
@@ -1,3 +1,15 @@
-function train() {
+function trainInit() {
+	train(false);
+	var initBus = setTimeout(function () {
+		train(true);
+	}, nextMinute());
+}
 
+function train(autoRefresh) {
+
+	if (autoRefresh) {
+		var busRefresh = setTimeout(function () {
+			bus(true);
+		}, nextMinute());
+	}
 }
