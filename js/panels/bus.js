55d23a13087c3d10ead00e83fc3ad28654e0687a
Zackary Corbett <zyber17@gmail.com>
2016-05-21 20:37:34 -0400

    Maybe fixed a bug where bus would trigger multiple times?

--- js/panels/bus.js
+++ js/panels/bus.js
@@ -31,9 +31,3 @@ function bus(autoRefresh) {
 		}, nextMinute());
 	}
 }
-
-function nextMinute() {
-	var now = new Date();
-	var time = (59 - now.getSeconds()) * 1000 + (1000 - now.getMilliseconds()); // Get time until next minute
-	return time;
-}
