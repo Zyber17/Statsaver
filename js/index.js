5cbfb3fbe8ac66fe802c865cea44cc1479aa51c7
Zackary Corbett <zyber17@gmail.com>
2016-05-23 00:09:50 -0400

    Removed some old code and test code.

--- js/index.js
+++ js/index.js
@@ -10,17 +10,9 @@ function init() {
 	noteInit();
 }
 
-// function nextMinute() {
-// 	var now = new Date();
-// 	var nextMin = (59 - now.getSeconds()) * 1000 + (1000 - now.getMilliseconds()); // Get time until next minute
-// 	return nextMin;
-// }
-
 function schedule(fun, mins) {
 	var now = new Date();
 	var till = (59 - now.getSeconds()) * 1000 + (1000 - now.getMilliseconds()) + (mins - 1) * 60 * 1000; // Get time until next minute
-
-	console.log('hello '+mins);
 
 	var timeout = setTimeout(function() {
 		fun();
