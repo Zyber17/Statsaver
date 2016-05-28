b093f87be1289f40afae2c13c1a3b608a05f33c0
Zackary Corbett <zyber17@gmail.com>
2016-05-28 18:39:20 -0400

    Shows rain if raining soon even if not now; notifies of sever weather alert;

--- js/panels/weather.js
+++ js/panels/weather.js
@@ -1,4 +1,4 @@
-var skycons, temp, high, low;
+var skycons, temp, high, low, wAlert;
 
 function weatherInit() {
 	var canvas = $('#skycon')[0];
@@ -7,7 +7,7 @@ function weatherInit() {
 	canvas.height = 256;
 	// canvas.style.width = "128px";
 	// canvas.style.height = "128px";
-	canvas.getContext('2d').scale(2,2);
+	//canvas.getContext('2d').scale(2,2);
 
 	skycons = new Skycons({
 		"monochrome": false,
@@ -33,16 +33,32 @@ function weatherInit() {
 }
 
 function weather() {
-	var url = 'https://api.forecast.io/forecast/' + window.forcastAPIKey + '/' + window.lat + ',' + window.long;
+	var url = 'https://api.forecast.io/forecast/' + window.forcastAPIKey + '/' + window.lat + ',' + window.long + '?exclude=hourly,flags';
+	console.log(url);
 	$.getJSON(url, function(data) {
 		temp.html(Math.round(data.currently.temperature));
 		high.html(Math.round(data.daily.data[0].temperatureMax));
 		low.html(Math.round(data.daily.data[0].temperatureMin));
-		// $('#precip').html(Math.round(data.currently.precipProbability * 100));
-		skycons.set('skycon', data.currently.icon);
-	});
 
-	schedule(function() {
-		weather();
-	}, 5);
+		var rainSoon = false;
+		for (var i = 0; i < 20; i++) {
+			if (data.minutely.data[i].precipProbability >= 0.25) {
+				rainSoon = true;
+				break;
+			}
+		}
+		skycons.set('skycon', (rainSoon ? 'rain' : data.currently.icon));
+
+		if ("alert" in data) {
+			$('#weatherAlert').fadeIn(1000);
+		} else {
+			$('#weatherAlert').fadeOut(1000);
+		}
+
+
+
+		schedule(function() {
+			weather();
+		}, 5);
+	});
 }
